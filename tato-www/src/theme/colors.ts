f139a0e1494b640178a740f2a7d5e4ee2ab117e1
LordPotato <patatemonnaie@gmail.com [00:36]>
2020-09-09 02:26:03 +0100

    Init

---
+++ tato-www/src/theme/colors.ts
@@ -0,0 +1,22 @@
+export const white = '#FFF'
+export const black = '#000'
+
+export const green = {
+  500: '#00D110',
+}
+
+export const red = {
+  100: '#FFFDFE',
+  200: '#FFA8C7',
+  500: '#02D3A3',
+}
+
+export const grey = {
+  100: '#195361',
+  200: '#f55a51',
+  300: '#195361',
+  400: '#195361',
+  500: '#195361',
+  600: '#195361', 
+  800: '#2D121B',
+}
